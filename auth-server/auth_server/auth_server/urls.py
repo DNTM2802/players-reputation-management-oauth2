0d436c5ad7f3b3d816cc0767c1c6fdd24833843b
botto <joselmdbsousa@ua.pt>
2022-05-30 15:52:08 +0100

    urls for register, login, logout

--- auth-server/auth_server/auth_server/urls.py
+++ auth-server/auth_server/auth_server/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views.views import *
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
