167bf67e8bbffbd216383281de89f9dc5b5d0e5d
dntm2802 <duarte.ntm@ua.pt>
2022-06-06 16:28:03 -0700

    [deprecated] oauth app for customizing the django-oauth-toolkit tokens

--- auth-server/auth_server/oauth/validator/CustomValidator.py
+++ auth-server/auth_server/oauth/validator/CustomValidator.py
@@ -2,36 +2,40 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 from oauth2_provider.settings import oauth2_settings
 
 
-class CustomValidator(OAuth2Validator):
-    def validate_bearer_token(self, token, scopes, request):
-        """
-        When users try to access resources, check that provided token is valid
-        """
-        print("Oiiiiiiiii")
-        if not token:
-            return False
 
-        introspection_url = oauth2_settings.RESOURCE_SERVER_INTROSPECTION_URL
-        introspection_token = oauth2_settings.RESOURCE_SERVER_AUTH_TOKEN
-        introspection_credentials = oauth2_settings.RESOURCE_SERVER_INTROSPECTION_CREDENTIALS
-
-        access_token = self._load_access_token(token)
-
-        # if there is no token or it's invalid then introspect the token if there's an external OAuth server
-        if not access_token or not access_token.is_valid(scopes):
-            if introspection_url and (introspection_token or introspection_credentials):
-                access_token = self._get_token_from_authentication_server(
-                    token, introspection_url, introspection_token, introspection_credentials
-                )
-
-        if access_token and access_token.is_valid(scopes):
-            request.client = access_token.application
-            request.user = access_token.user
-            request.scopes = scopes
-
-            # this is needed by django rest framework
-            request.access_token = access_token
-            return True
-        else:
-            self._set_oauth2_error_on_request(request, access_token, scopes)
-            return False
+# class CustomValidator(OAuth2Validator):
+#     def validate_bearer_token(self, token, scopes, request):
+#         """
+#         When users try to access resources, check that provided token is valid
+#         """
+#         print("Oiiiiissiiii")
+#         print(scopes)
+#         if not token:
+#             return False
+#
+#         introspection_url = oauth2_settings.RESOURCE_SERVER_INTROSPECTION_URL
+#         introspection_token = oauth2_settings.RESOURCE_SERVER_AUTH_TOKEN
+#         introspection_credentials = oauth2_settings.RESOURCE_SERVER_INTROSPECTION_CREDENTIALS
+#
+#         access_token = self._load_access_token(token)
+#
+#         # if there is no token or it's invalid then introspect the token if there's an external OAuth server
+#         if not access_token or not access_token.is_valid(scopes):
+#             if introspection_url and (introspection_token or introspection_credentials):
+#                 access_token = self._get_token_from_authentication_server(
+#                     token, introspection_url, introspection_token, introspection_credentials
+#                 )
+#
+#         if access_token and access_token.is_valid(scopes):
+#             request.client = access_token.application
+#             request.user = access_token.user
+#             request.scopes = scopes
+#
+#             # this is needed by django rest framework
+#             request.access_token = access_token
+#             print("here custom")
+#             return True
+#         else:
+#             self._set_oauth2_error_on_request(request, access_token, scopes)
+#             print("there custom")
+#             return False
