af1c1d35f13edb42d57dd0f2df7c7b6f7632cea2
l-sousa <joselmdbsousa@ua.pt>
2022-06-10 17:50:25 +0100

    updated user agent to all flask communications

--- user_agent/run.py
+++ user_agent/run.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+
 app = Flask(__name__, template_folder='templates/')
 
-# secrets: client id 
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        game = request.form["game"]
+        skill = request.form["skill"]
+        behaviour = request.form["behaviour"]
+        granularity = request.form["granularity"]
 
-@app.route('/')
-def hello_world():
+        # print(game, skill, behaviour, granularity)
+        return redirect(
+            f"http://localhost:8002/play_game?game={game}&skill={skill}&behaviour={behaviour}&granularity={granularity}&return=http://localhost:8003/match")
+
     return render_template('index.html')
 
-# endpoint rewquest authgrant
-# endpoint excahgne para pedir o access token 
+
+@app.route("/match", methods=["GET"])
+def match():
+    return render_template("match.html", results=request.args.get("results"),
+                           reputation=request.args.get("reputation"),
+                           behaviour=request.args.get("behaviour"))
 
 
 if __name__ == '__main__':
