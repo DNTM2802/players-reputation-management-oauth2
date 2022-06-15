9d195792678cf02ab9e97837cd50ddbc8f9cf2e9
l-sousa <joselmdbsousa@ua.pt>
2022-06-15 04:41:33 +0100

    Revert "config files for django channels"
    This reverts commit dcb80ba4945fd8722310c7ada2281b45444756d7.

--- tables_matchmaker/matchmaker/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-  re_path(r'ws/matchmaker/(?P<room_id>\w+)/$', consumers.Consumer.as_asgi()),
-]
